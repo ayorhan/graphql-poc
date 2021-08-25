0fd85221fe3df0a09e944150539d01b3a394c59f
Arda Orhan <arda.orhan@devfactory.com>
2021-08-26 00:18:38 +0300

    added mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://arda-poc:arda-poc@cluster0.hffqk.mongodb.net/Cluster0?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('Mongo connected!')
+});
 
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
